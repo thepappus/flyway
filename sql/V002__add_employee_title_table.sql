788f44757a5024a2f284af2aed12ed1341152409
pappuvishal <thepappus@yahoo.com>
2018-06-24 06:01:52 +0900

    adding title ecords

--- sql/V002__add_employee_title_table.sql
+++ sql/V002__add_employee_title_table.sql
@@ -3,3 +3,8 @@ CREATE TABLE titles (
   title VARCHAR(100) NOT NULL, 
   PRIMARY KEY (id) 
 )Engine=InnoDB; 
+
+insert into titles(title) values("Underling");
+insert into titles(title) values("Evil Warlord");
+insert into titles(title) values("Benevolent Dictator");
+
